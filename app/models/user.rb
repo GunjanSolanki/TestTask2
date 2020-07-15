8e815a152da3d9b0e5800242360e7ef5071127ea
Gunjan Solanki <gsolanki@techracers.io>
2020-07-15 18:07:30 +0530

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  enum role: [:user, :admin]
+
+  validates :name, presence: true, length: { maximum: 10 } 
+  validates :email, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  validates :contact, presence: true, length: { maximum: 10 }
+
   has_many :products
 end
