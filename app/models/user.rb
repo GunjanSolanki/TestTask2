b30f6eacf646404e0e710047fed548251832126f
Gunjan Solanki <gsolanki@techracers.io>
2020-07-15 18:14:13 +0530

    feat: Add devise model, migration, controller, routes views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   enum role: [:user, :admin]
-
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+  
   validates :name, presence: true, length: { maximum: 10 } 
   validates :email, uniqueness: true
   validates :password, presence: true, confirmation: true
