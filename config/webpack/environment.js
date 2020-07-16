441071ebeae9269ab72153c6aff85f9cc3413954
Gunjan Solanki <gsolanki@techracers.io>
2020-07-16 22:29:59 +0530

    fix: add jquery configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,6 +4,10 @@ const webpack = require("webpack")
 
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
 
+$: 'jquery',
+
+jQuery: 'jquery',
+
 Popper: ['popper.js', 'default']
 
 }))
