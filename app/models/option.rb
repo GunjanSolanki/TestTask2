5cc1f8d1c63de8dc67281ccf6a01c89d3fc9644e
Gunjan Solanki <gsolanki@techracers.io>
2020-07-16 21:28:43 +0530

    Add validation for Model option

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,5 +2,5 @@ class Option < ApplicationRecord
   has_many :variant_options, dependent: :destroy
   has_many :variants, through: :variant_options
 
-  has_many :products, dependent: :destroy
+  validates :option_type, uniqueness: true
 end
