bdaffa4981e790b5be507910eb8b69f6cfd03012
Gunjan Solanki <gsolanki@techracers.io>
2020-07-16 21:44:28 +0530

    fix: Change option_value field to type array

--- db/migrate/20200714132316_create_options.rb
+++ db/migrate/20200714132316_create_options.rb
@@ -2,7 +2,7 @@ class CreateOptions < ActiveRecord::Migration[6.0]
   def change
     create_table :options do |t|
       t.string :option_type
-      t.string :option_value
+      t.string :option_value, array: true, default: []
 
       t.timestamps
     end
