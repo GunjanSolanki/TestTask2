1cb938f107ed3453935f1cf09bff0c56d2129f54
Gunjan Solanki <gsolanki@techracers.io>
2020-07-16 21:32:18 +0530

    Add validation for Variant model

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -3,4 +3,7 @@ class Variant < ApplicationRecord
 
   has_many :variant_options, dependent: :destroy
   has_many :options, through: :variant_options
+  accepts_nested_attributes_for :options
+
+  validates :sku, uniqueness: true
 end
